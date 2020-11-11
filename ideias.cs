b864759c25ac1e92ef194196173a5123dbd7172c
DaanielSouzaa <danielavelino@ucl.br>
2020-11-11 00:25:44 +0000

    50%

--- ideias.cs
+++ ideias.cs
@@ -7,6 +7,7 @@ class ideias {
   private List<string> areaAtuacao = new List<string>();
   private List<string> autor = new List<string>();
   private List<double> valorMinNecessario = new List<double>();
+  private List<int> votos = new List<int>();
 
   public void setIdeia(string a,string b,string c,double d){
     this.descricao.Add(a);
@@ -16,14 +17,17 @@ class ideias {
   }
 
   public void getIdeia(){
-    Console.WriteLine(descricao[0]);
+    for(int i = 0;i < this.valorMinNecessario.Count;i++){
+      Console.WriteLine("Ideia | Descrição | Área | Autor");
+      Console.WriteLine("{0} | {1} | {2} | {3}",i,this.descricao[i],this.areaAtuacao[i],this.autor[i]);
+    }
   }
 
   public bool cadIdeia(){
-    string desc;
-    string area;
-    string autor;
-    double valorMin;
+    string desc = "";
+    string area = "";
+    string autor = "";
+    string valorMin = "";
     string confirm;
 
     while (desc == "" || area == "" || autor == "" || valorMin == ""){
@@ -31,27 +35,29 @@ class ideias {
         if (desc == ""){
           Console.WriteLine("Digite a descrição de sua ideia!");
           desc = Console.ReadLine();
-        };
+        }
 
         if (area == ""){
           Console.WriteLine("Digite a área de atuação de sua ideia!");
           area = Console.ReadLine();
-        };
+        }
 
         if (autor == ""){
           Console.WriteLine("Digite seu nome:");
           autor = Console.ReadLine();
-        };
+        }
 
         if (valorMin == ""){
           Console.WriteLine("Digite o valor mínimo para que sua ideia seja executada!");
-          valorMin = double.Parse(Console.ReadLine());
-        };
+          valorMin = Console.ReadLine();
+        }
 
         Console.WriteLine("Digite 's' para cadastrar outra ideia ou qualquer outra tecla para sair!");
         confirm = Console.ReadLine();
+
+        double valorConvert = double.Parse(valorMin);
         
-        setIdeia(desc,area,autor,valorMin);
+        setIdeia(desc,area,autor,valorConvert);
 
         if (confirm != "s"){
           return false;
@@ -61,7 +67,21 @@ class ideias {
       {
         Console.WriteLine("Valor digitado inválido!");
       }
-      return true;
+    }
+    return true;
+  }
+
+  public void curtir(int index){
+    int indexVotado;
+
+    try{
+      Console.WriteLine("Digite o número da ideia que deseja votar!");
+      indexVotado = int.Parse(Console.ReadLine());
+      this.votos.Add(indexVotado);
+    } catch(FormatException){
+      Console.WriteLine("Valor inválido!");
     }
   }
+
+
 }
